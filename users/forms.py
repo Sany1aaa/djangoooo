272a1e5588e9583defe3aeedfd32a04881b08090
Сания <saniya@MacBook-Pro-Sania.local>
2024-10-09 11:09:05 +0500

    haha

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+
+
+from django import forms
+from .models import Profile, Follow, User
+
+class ProfileForm(forms.ModelForm):
+    class Meta: 
+        model = Profile 
+        fields ="__all__"
+
+class FollowForm(forms.ModelForm): 
+    class Meta: 
+        model = Follow 
+        fields = "__all__"
+
+class UserForm(forms.ModelForm): 
+    class Meta: 
+        model = User 
+        fields = ['username', 'email', 'password']
